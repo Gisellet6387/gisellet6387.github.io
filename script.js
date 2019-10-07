b99ce360af6bfdfc4e7ed3c487d8dfc4a90e024e
Gisellet6387 <43858670+Gisellet6387@users.noreply.github.com>
2019-10-07 10:29:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Giselle"
+let userName = "gisellet6387";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
